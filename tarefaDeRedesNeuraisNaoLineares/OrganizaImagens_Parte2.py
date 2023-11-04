7b182902f30e6158ad5ba1e7330348f52d743e25
BrunoPNeves <bruno.neves9@unifor.br>
2023-11-04 14:32:56 -0300

    Leitura dos dados e criando as funcoes de MLP

--- tarefaDeRedesNeuraisNaoLineares/OrganizaImagens_Parte2.py
+++ tarefaDeRedesNeuraisNaoLineares/OrganizaImagens_Parte2.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-folderRoot = 'faces\\faces\\' #MODIFIQUE para o caminho do conjunto de dados no seu computador.
+folderRoot = 'faces/faces/'
 individual = ['an2i','at33','boland', 'bpm', 'ch4f', 'cheyer', 'choon', 'danieln', 'glickman', 'karyadi', 'kawamura', 'kk49', 'megak', 'mitchell', 'night', 'phoebe', 'saavik', 'steffi', 'sz24', 'tammo'] #os 20 sujeitos no conjunto de dados.
 expressoes =['_left_angry_open', '_left_angry_sunglasses', '_left_happy_open','_left_happy_sunglasses', '_left_neutral_open', '_left_neutral_sunglasses', '_left_sad_open', '_left_sad_sunglasses', '_right_angry_open', '_right_angry_sunglasses', '_right_happy_open', '_right_happy_sunglasses', '_right_neutral_open', '_right_neutral_sunglasses', '_right_sad_open', '_right_sad_sunglasses', '_straight_angry_open', '_straight_angry_sunglasses', '_straight_happy_open', '_straight_happy_sunglasses', '_straight_neutral_open', '_straight_neutral_sunglasses', '_straight_sad_open', '_straight_sad_sunglasses' ,'_up_angry_open', '_up_angry_sunglasses', '_up_happy_open', '_up_happy_sunglasses', '_up_neutral_open', '_up_neutral_sunglasses', '_up_sad_open', '_up_sad_sunglasses'] 
 QtdIndividuos = len(individual)
@@ -12,7 +12,7 @@ Y = np.empty((QtdIndividuos,0))
 
 for i in range(QtdIndividuos):
     for j  in range(QtdExpressoes):
-        path = folderRoot+individual[i]+'\\'+individual[i]+expressoes[j]+'.pgm'
+        path = folderRoot+individual[i]+'/'+individual[i]+expressoes[j]+'.pgm'
         PgmImg = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
         ResizedImg =cv2.resize(PgmImg,(Red,Red))
         
